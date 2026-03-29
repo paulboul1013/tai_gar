c2bc4878e3cb28e252aded45cd1d809fd0c1856e
paulboul1013 <paulboul1013@gmail.com>
2026-03-29 22:14:46 +0800

    Add DrawText\DrawRect class

--- browser.py
+++ browser.py
@@ -106,6 +106,41 @@ def paint_tree(layout_object,display_list):
     for child in layout_object.children:
         paint_tree(child,display_list)
 
+class DrawText:
+    def __init__(self,x1,y1,text,font):
+        self.left=x1
+        self.top=y1
+        self.text=text
+        self.font=font
+        self.bottom=y1+font.metrics("linespace")
+
+    def execute(self,scroll,canvas):
+        canvas.create_text(
+            self.left,
+            self.top-scroll,
+            text=self.text,
+            font=self.font,
+            anchor="nw",
+        )
+
+class DrawRect:
+    def __init__(self,x1,y1,x2,y2,color):
+        self.left=x1
+        self.top=y1
+        self.right=x2
+        self.bottom=y2
+        self.color=color
+
+    def execute(self,scroll,canvas):
+        canvas.create_rectangle(
+            self.left,
+            self.top-scroll,
+            self.right,
+            self.bottom-scroll,
+            width=0,
+            fill=self.color,
+        )
+
 class DocumentLayout:
     def __init__(self,node):#build root of layout tree
         self.node=node
