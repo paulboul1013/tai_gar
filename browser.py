f9db84c77c278f807c8770ba59ff229cf13b48d9
paulboul1013 <paulboul1013@gmail.com>
2026-06-01 14:54:55 +0800

    build full linelayout class

--- browser.py
+++ browser.py
@@ -212,6 +212,47 @@ class LineLayout:
         self.previous=previous
         self.children=[]
 
+        self.x=None
+        self.y=None
+        self.width=None
+        self.height=None
+
+    def layout(self):
+        self.width=self.parent.width
+        self.x=self.parent.x
+
+        if self.previous:
+            self.y = self.previous.y + self.previous.height
+        else:
+            self.y=self.parent.y
+        
+        for word in self.children:
+            word.layout()
+
+        if not self.children:
+            self.height=0
+            return
+
+        max_ascent = max([
+            word.font.metrics("ascent")
+            for word in self.children
+        ])
+
+        baseline = self.y+1.25*max_ascent
+
+        for word in self.children:
+            word.y=baseline-word.font.metrics("descent")
+
+        max_descent = max([
+            word.font.metrics("descent")
+            for word in self.children
+        ])
+
+        self.height=1.25 *(max_ascent+max_descent)
+
+    def paint(self):
+        return []
+
 class TextLayout:
     def __init__(self,node,word,parent,previous):
         self.node=node
