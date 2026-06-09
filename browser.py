5870b336c7eec5c531551565fd701190ffdbc270
paulboul1013 <paulboul1013@gmail.com>
2026-06-09 09:45:22 +0800

    dynamic calculate every tabs position

--- browser.py
+++ browser.py
@@ -1175,6 +1175,39 @@ def cascade_priority(rule):
     selector, body=rule
     return selector.priority
 
+class Chrome:
+    def __init__(self,browser):
+        self.browser=browser
+
+        self.font=get_font(20,"normal","roman")
+        self.font_height=self.font.metrics("linespace")
+
+        self.padding=5
+
+        self.tabbar_top=0
+        self.tabbar_bottom=self.font_height+2*self.padding
+
+        plus_width=self.font.measure("+") + 2*self.padding
+
+        self.newtab_rect = Rect(
+            self.padding,
+            self.padding,
+            self.padding+plus_width,
+            self.padding+self.font_height
+        )
+
+        self.bottom = self.tabbar_bottom
+
+    def tab_rect(self,i):
+        tabs_start=self.newtab_rect.right+self.padding
+        tab_width=self.font.measure("Tab X")+2*self.padding
+        return Rect(
+            tabs_start+tab_width*i,
+            self.tabbar_top,
+            tabs_start+tab_width*(i+1),
+            self.tabbar_bottom
+        )
+
 class Tab:
     def __init__(self):
         # self.window = tkinter.Tk()
