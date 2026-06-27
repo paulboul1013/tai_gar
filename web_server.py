2f0e977f30b6eafcb1ccb4a57ed9fd02971f69c3
paulboul1013 <paulboul1013@gmail.com>
2026-06-27 11:52:32 +0800

    Add test server

---
+++ web_server.py
@@ -0,0 +1,41 @@
+import socket
+
+s = socket.socket(
+    family=socket.AF_INET,
+    type=socket.SOCK_STREAM,
+    proto=socket.IPPROTO_TCP)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind(('', 8000))
+s.listen()
+
+def handle_connection(conx):
+    req = conx.makefile("b")
+    reqline = req.readline().decode('utf8')
+    method, url, version = reqline.split(" ", 2)
+    assert method in ["GET", "POST"]
+
+    headers = {}
+    while True:
+        line = req.readline().decode('utf8')
+        if line == '\r\n': break
+        header, value = line.split(":", 1)
+        headers[header.casefold()] = value.strip()
+
+    if 'content-length' in headers:
+        length = int(headers['content-length'])
+        body = req.read(length).decode('utf8')
+    else:
+        body = None
+
+    status, body = do_request(method, url, headers, body)
+    response = "HTTP/1.0 {}\r\n".format(status)
+    response += "Content-Length: {}\r\n".format(
+    len(body.encode("utf8")))
+    response += "\r\n" + body
+    conx.send(response.encode('utf8'))
+    conx.close()
+
+while True:
+    conx, addr = s.accept()
+    handle_connection(conx)
