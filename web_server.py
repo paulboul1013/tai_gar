9cf62264db112ca3b37cd595f10aedf49d7a86ae
paulboul1013 <paulboul1013@gmail.com>
2026-06-27 12:01:00 +0800

    change socket process

--- web_server.py
+++ web_server.py
@@ -1,13 +1,5 @@
 import socket
 
-s = socket.socket(
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-    proto=socket.IPPROTO_TCP)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-s.bind(('', 8000))
-s.listen()
 
 ENTRIES = [ 'Pavel was here' ]
 
@@ -39,6 +31,16 @@ def handle_connection(conx):
     conx.send(response.encode('utf8'))
     conx.close()
 
-while True:
-    conx, addr = s.accept()
-    handle_connection(conx)
+if __name__ == "__main__":
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind(('', 8000))
+    s.listen()
+
+    while True:
+        conx, addr = s.accept()
+        handle_connection(conx)
