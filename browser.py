0fd47d42b0b593af1e717ea62e1e79d2e194dc32
paulboul1013 <paulboul1013@gmail.com>
2026-06-30 12:53:03 +0800

    Add ButtonContentParent for button internel content fake parent

--- browser.py
+++ browser.py
@@ -614,6 +614,11 @@ class InputLayout:
 
         return cmds
         
+class ButtonContentParent:
+    def __init__(self,x,y,width):
+        self.x=x
+        self.y=y
+        self.width=width
 
 class ButtonLayout:
     def __init__(self,node,parent,previous):
