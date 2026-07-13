d68263faa8113d0d105db4b5be86d8cf9c3968f4
paulboul1013 <paulboul1013@gmail.com>
2026-07-13 11:43:31 +0800

    Add void elements to avoid auto close tag

--- browser.py
+++ browser.py
@@ -41,14 +41,11 @@ BUTTON_PADDING = 4
 
 USE_RTL=False
 
-# BLOCK_ELEMENTS = [
-#     "html", "body", "article", "section", "nav", "aside",
-#     "h1", "h2", "h3", "h4", "h5", "h6", "hgroup", "header",
-#     "footer", "address", "p", "hr", "pre", "blockquote",
-#     "ol", "ul", "menu", "li", "dl", "dt", "dd", "figure",
-#     "figcaption", "main", "div", "table", "form", "fieldset",
-#     "legend", "details", "summary"
-# ]
+VOID_ELEMENTS = {
+    "area", "base", "br", "col", "embed",
+    "hr", "img", "input", "link", "meta",
+    "param", "source", "track", "wbr",
+}
 
 def normalize_tk_weight(weight):
     if weight in ["normal","bold"]:
